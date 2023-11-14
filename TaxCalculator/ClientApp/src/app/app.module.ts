d268b7fd058dcf8b977072093032bd74ee52ff74
Svitozar Ostapchuk <Svitozar_Ostapchuk@epam.com>
2023-11-14 21:50:57 +0100

    FE: removed unnecessary code

--- TaxCalculator/ClientApp/src/app/app.module.ts
+++ TaxCalculator/ClientApp/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { APP_INITIALIZER } from '@angular/core';
-import { AppConfigService } from '../../src/app/services/app-config-service.service';
 import { TaxCalculatorModule } from './tax-calculator/tax-calculator.module';
 
 
@@ -18,14 +16,7 @@ import { TaxCalculatorModule } from './tax-calculator/tax-calculator.module';
     HttpClientModule,
     TaxCalculatorModule,
   ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: (config: AppConfigService) => () => config.loadConfig(),
-      deps: [AppConfigService],
-      multi: true
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 
